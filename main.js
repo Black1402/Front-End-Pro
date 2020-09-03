250445b6fee7604af6cab4ae639b2bc2826027a8
Black1402 <37223738+Black1402@users.noreply.github.com>
2020-09-03 15:56:04 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 //Lesson-1, part-1
+let star = '*'.repeat(14);
+console.log(star);
 
-stars = '**************';
-list = 'Список студентов зарегестрировавшихся на вебинар:';
-myAge = '1. 18 лет Юрий Иванов';
-myAgeTwo = '2. 16 лет Дмитрий Кузьмин';
-myAgeThree = '3. 19 лет Оксана Петрова';
-middleAge = '-------------- Средний возраст студента: [средний возраст студента] --------------';
-star = '**************';
-
-console.log(stars);
+let list = 'Список студентов зарегестрировавшихся на вебинар:';
 console.log(list);
+
+let myAge = '1. 18 лет Юрий Иванов';
 console.log(myAge);
-console.log(myAgeTwo);
-console.log(myAgeThree);
+
+myAge = '2. 16 лет Дмитрий Кузьмин';
+console.log(myAge);
+
+myAge = '3. 19 лет Оксана Петрова';
+console.log(myAge);
+
+let middleAge = '-'.repeat(14).concat("'Средний возраст студента: [средний возраст студента]'") + '-'.repeat(14);
 console.log(middleAge);
-console.log(stars);
+
+console.log(star);
 
 //Lesoon-1, part-2
 
@@ -40,3 +43,7 @@ console.log(c);
 
 c = a / b;
 console.log(c);
+
+
+
+
